39024c3bd13ecb325c4cdf643aa193f6c0700e15
川口拓巳 <kawaguchitakumi@kawaguchitakuminoMacBook-Pro.local>
2017-12-19 15:20:38 +0900

    message送信機能実装

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+    @message  = Message.new
+  end
+
+  def create
+    @message = Message.create(message_params)
+    if @message.save
+      redirect_to talk_messages_path
+    else
+      render 'index'
+    end
+  end
+
+private
+  def message_params
+    params.require(:message).permit(:text, :talk_id, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'general#home'
-  resources :talks
+  resources :talks do 
+    resources :messages, only: [:index, :new, :create]
+  end
   get 'general/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
